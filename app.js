0a2d53975093612377fb2b868b5af849bcccd0f2
Arthur <aarthurrrodrigues99@gmail.com>
2025-08-03 18:58:17 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafiooo';
+
+function Console (){
+    console.log('O botão foi clicado');
+}
+function Alerta(){alert('Eu amo JS');}
+let cidade;
+function Prompt(){cidade = prompt('Um nome de uma cidade no Brasil');
+alert(`Eu estive em ${cidade} e lembrei de você`);};
+
+
+function Soma() {
+    let numero1 = Number(prompt('Escolha um número'));
+let numero2 = Number(prompt('Escolha outro número'));
+let resultado = numero1 + numero2;
+    
+
+alert(`${resultado} esse é o resultado da soma dos números`);
+};
